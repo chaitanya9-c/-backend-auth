5e2974acd9af8b0e1b1a851057dada75dc5375a6
Chaitanya <chaitanyagutha99@gmail.com>
2026-07-03 17:03:27 +0530

    port change

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,3 +11,5 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+
+module.exports = pool;
